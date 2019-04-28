cb4321ee5f40b115a98f4205bb74269bd186dde3
kisc <nuno_kisc@hotmail.com>
2019-04-28 04:08:02 +0100

    translate pt, add locale pt

---
+++ locales/pt.lua
@@ -0,0 +1,13 @@
+Locales['pt'] = {
+  ['new_message'] = '~b~Nova mensagem :~s~ %s',
+  ['press_take_call'] = '%s - Pressione ~INPUT_CONTEXT~ para aceitar a chamada',
+  ['taken_call'] = '~y~%s~s~ recebeu uma chamada',
+  ['gps_position'] = 'destino inserido no GPS',
+  ['message_sent'] = 'mensagem enviada',
+  ['cannot_add_self'] = 'não te podes adicionar',
+  ['number_in_contacts'] = 'este número já está lista de contatos',
+  ['contact_added'] = 'contacto adicionado',
+  ['contact_removed'] = 'contacto removido!',
+  ['number_not_assigned'] = 'este número não foi atribuído...',
+  ['invalid_number'] = 'este numero não é valido!',
+}
